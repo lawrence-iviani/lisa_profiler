e0f5a8b1ad405f9eef706cd60f91348c0fa4d698
Lawrence Iviani <lawrence.iviani@gmail.com>
2020-12-11 18:31:14 -0500

    Define file reading, buffering and reproduction

--- scripts/player.py
+++ scripts/player.py
@@ -15,97 +15,102 @@ but this example shows what can be done when NumPy is not available.
 example from https://github.com/spatialaudio/python-sounddevice/blob/0.4.1/examples/play_long_file.py
 
 """
+
+from lisa_profiler import AudioParametersStruct, read_file_and_play,  query_devices
+from lisa_profiler import get_test_token
+
+
 import argparse
 import queue
 import sys
-import threading
-
-import sounddevice as sd
-import soundfile as sf
+# todo: replace with ros sleep
+from time import sleep
 
 
 def int_or_str(text):
-    """Helper function for argument parsing."""
-    try:
-        return int(text)
-    except ValueError:
-        return text
+	"""Helper function for argument parsing."""
+	try:
+		return int(text)
+	except ValueError:
+		return text
 
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    '-l', '--list-devices', action='store_true',
-    help='show list of audio devices and exit')
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser])
-parser.add_argument(
-    'filename', metavar='FILENAME',
-    help='audio file to be played back')
-parser.add_argument(
-    '-d', '--device', type=int_or_str,
+if __name__ == "__main__":
+	print("Starting with python {}".format( sys.version_info))
+	if sys.version_info[0] < 3:
+		raise Exception("Must be using Python 3")
+
+	parser = argparse.ArgumentParser(add_help=False)
+	parser.add_argument(
+		'-l', '--list-devices', action='store_true',
+		help='show list of audio devices and exit')
+	args, remaining = parser.parse_known_args()
+	if args.list_devices:
+		print(query_devices())
+		parser.exit(0)
+	parser = argparse.ArgumentParser(
+    	description=__doc__,
+    	formatter_class=argparse.RawDescriptionHelpFormatter,
+		parents=[parser])
+	parser.add_argument(
+		'filename', metavar='FILENAME',
+		help='JSON audio description of the playback')
+	parser.add_argument(
+		'-d', '--device', type=int_or_str,
     help='output device (numeric ID or substring)')
-parser.add_argument(
-    '-b', '--blocksize', type=int, default=2048,
-    help='block size (default: %(default)s)')
-parser.add_argument(
-    '-q', '--buffersize', type=int, default=20,
-    help='number of blocks used for buffering (default: %(default)s)')
-args = parser.parse_args(remaining)
-if args.blocksize == 0:
-    parser.error('blocksize must not be zero')
-if args.buffersize < 1:
-    parser.error('buffersize must be at least 1')
+	parser.add_argument(
+		'-b', '--blocksize', type=int, default=2048,
+		help='block size (default: %(default)s)')
+	parser.add_argument(
+		'-q', '--buffersize', type=int, default=15,
+		help='number of blocks used for buffering (default: %(default)s)')
+	args = parser.parse_args(remaining)
+	if args.blocksize == 0:
+		parser.error('blocksize must not be zero')
+	if args.buffersize < 1:
+		parser.error('buffersize must be at least 1')
 
-q = queue.Queue(maxsize=args.buffersize)
-event = threading.Event()
+	
+	audio_params = AudioParametersStruct( device=args.device, buffersize=args.buffersize, blocksize=args.blocksize)
+	# open instead of this snippet creating from: args.filename
+	json_tests_list = []
+	json_tests_list.append(get_test_token(wave_folder='/media/sf_Thesis/test track', 
+								   args_wakeup={'expected_wakeup_word': 'snowboy', 'filename': 'snowboy_test_1.wav'}, # 'long.wav'}, # 'snowboy_test_1.wav'}, 
+								   args_intent={'expected_intents': ['set_acceleration'], 'filename': 'intent_test_1.wav'}))
+	json_tests_list.append(get_test_token(wave_folder='/media/sf_Thesis/test track', 
+								   args_wakeup={'expected_wakeup_word': 'snowboy', 'filename': 'snowboy_test_2.wav'}, 
+								   args_intent={'expected_intents': ['set_acceleration'], 'filename': 'intent_test_2.wav'}))
+
+	def _run_test(filename, pause_before, pause_after, buffersize):
+		
+		print("sleeping BEFORE for {} sec.".format(pause_before ))
+		sleep(pause_before)
+		
+		# TODO: emit ros message start wakeup reproduction
+		print("Play file {}".format(filename))
+		read_file_and_play(filename, audio_params, buffer_queue_size=buffersize )
+		
+		# TODO: wakeup expected, emit ros message end wakeup reproduction
+		print("sleeping AFTER for {} sec.".format(pause_after ))
+		sleep(pause_after)
 
 
-def callback(outdata, frames, time, status):
-    assert frames == args.blocksize
-    if status.output_underflow:
-        print('Output underflow: increase blocksize?', file=sys.stderr)
-        raise sd.CallbackAbort
-    assert not status
-    try:
-        data = q.get_nowait()
-    except queue.Empty as e:
-        print('Buffer is empty: increase buffersize?', file=sys.stderr)
-        raise sd.CallbackAbort from e
-    if len(data) < len(outdata):
-        outdata[:len(data)] = data
-        outdata[len(data):] = b'\x00' * (len(outdata) - len(data))
-        raise sd.CallbackStop
-    else:
-        outdata[:] = data
+	try:
+		for t in json_tests_list:
+			print("===== START TEST ITEM ====\n=============================\n")
+			# wakeup
+			print("===== WAKEUP ====")
+			_run_test(t['wakeup']['filename'], t['wakeup']['pause_before'], t['wakeup']['pause_after'], args.buffersize)
+			# sleep(2)
 
+			#####
+			print("\n===== INTENT ====")
+			_run_test(t['intent']['filename'], t['intent']['pause_before'], t['intent']['pause_after'], args.buffersize)
 
-try:
-    with sf.SoundFile(args.filename) as f:
-        for _ in range(args.buffersize):
-            data = f.buffer_read(args.blocksize, dtype='float32')
-            if not data:
-                break
-            q.put_nowait(data)  # Pre-fill queue
-        stream = sd.RawOutputStream(
-            samplerate=f.samplerate, blocksize=args.blocksize,
-            device=args.device, channels=f.channels, dtype='float32',
-            callback=callback, finished_callback=event.set)
-        with stream:
-            timeout = args.blocksize * args.buffersize / f.samplerate
-            while data:
-                data = f.buffer_read(args.blocksize, dtype='float32')
-                q.put(data, timeout=timeout)
-            event.wait()  # Wait until playback is finished
-except KeyboardInterrupt:
-    parser.exit('\nInterrupted by user')
-except queue.Full:
-    # A timeout occurred, i.e. there was an error in the callback
-    parser.exit(1)
-except Exception as e:
-    parser.exit(type(e).__name__ + ': ' + str(e))
+			print("===== END TEST ITEM ====\n=============================\n")
+
+	except KeyboardInterrupt:
+		parser.exit('\nInterrupted by user')
+	except Exception as e:
+		parser.exit('\nError catched: ' + type(e).__name__ + ': ' + str(e))
+
---
+++ src/lisa_profiler/__init__.py
@@ -0,0 +1,189 @@
+						
+import sys		
+from collections import namedtuple
+import sounddevice as sd
+import soundfile as sf
+import json
+import os.path
+import queue
+import threading
+
+# only temporary for debug
+from datetime import datetime
+
+AudioParametersStruct = namedtuple('AudioParametersStruct', ['device', 'buffersize', 'blocksize'])
+
+
+def query_devices():
+	return sd.query_devices()
+
+def _print_debug(msg='', function="", intent_level=0):
+	#datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+	msg = "[" + datetime.utcnow().strftime('%M:%S.%f')[:-3] + "]"+ intent_level * '\t' + function + "> " + msg
+	print(msg, file=sys.stderr)
+	
+
+def read_file_and_play(filename, audio_params, buffer_queue_size): #, data_queue, event):
+	IntenalStruct = namedtuple('IntenalStruct', ['max_retry', 'already_exiting' ])
+	# need to share with the play_callback
+	global data_queue, event, sample_per_block, duration_per_block, latency
+	
+	data_queue = queue.Queue(maxsize=buffer_queue_size)
+	event = threading.Event()
+
+	# these are init later
+	internal_data = IntenalStruct(max_retry = 10, already_exiting=False)
+	sample_per_block = 1 
+	duration_per_block = 1 
+	latency = 0
+
+
+	def play_callback(outdata, frames, time, status):
+		#_print_debug(function='play_callback', intent_level=2, msg='ENTER --- time={}, frames_to_play={} - status=>{}<'.format(time, frames, status))
+		assert frames == audio_params.blocksize, "Mismatch in frames number (received {} frames) expected are {}".format(frames, audio_params.blocksize)
+		if status.output_underflow:
+			# TODO: during stop this can happen
+			if internal_data.already_exiting:
+				return
+			_print_debug(function='play_callback', intent_level=2, msg='EXIT --- Output underflow: increase blocksize?')
+			raise sd.CallbackAbort
+		assert not status
+		try:
+			data = data_queue.get_nowait()
+			#_print_debug(function='play_callback', intent_level=2, msg='got data={} from {} which size is {}'.format(data, data_queue, data_queue.qsize()))
+			_print_debug(function='play_callback', intent_level=2, msg='got data chunk of {} msec.'.format(1000*duration_per_block*len(data)/len(outdata)))
+			len(data)
+		except queue.Empty as e:
+			if internal_data.already_exiting:
+				return
+			_print_debug(function='play_callback', intent_level=2, msg='Buffer is empty: increase buffersize?')
+			_print_debug(function='play_callback', intent_level=2, msg='queue.Empty! RETRY='+str(internal_data.max_retry))
+			if internal_data.max_retry==0:		
+				_print_debug(function='play_callback', intent_level=2, msg='EXIT EMPTY')
+				raise sd.CallbackAbort from e
+			internal_data.max_retry -= 1
+			return
+
+		if len(data) < len(outdata):
+			_print_debug(function='play_callback', intent_level=2, msg='prepare for stop len(data)={} < len(outdata)={}'.format( len(data) , len(outdata) ))
+			# The final data are copied to the sound card ...
+			outdata[:len(data)] = data
+			outdata[len(data):] = b'\x00' * (len(outdata) - len(data))
+			# ... and wait before the stop, otherwise it cuts the latest reproduction!
+			# ... or get an exception... 
+			# somehow the sum of the latest chunk plus the latency for reproduction. Empiric but seems work
+			already_exiting = True
+			wait_ms = int((latency + duration_per_block)*1000)
+			_print_debug(function='play_callback', intent_level=2, msg='EXIT STOP! wait {} msec.'.format(wait_ms))
+			sd.sleep(wait_ms) # in msec
+		else:
+			#_print_debug(function='play_callback', intent_level=2, msg='EXIT outdata regular {}'.format(data))
+			outdata[:] = data
+
+	def play_finished():
+		_print_debug(function="play_finished", intent_level=3, msg="event.set() {}".format(event))
+		event.set()
+		_print_debug(function="play_finished", intent_level=3, msg="exit")
+
+	_print_debug(function='read_file_and_play', msg="Open file: {}".format(sf.info(filename)) )
+	with sf.SoundFile(filename) as f:
+		_file_chunk_size = 2 # TODO: this must be derived from the format, now we assume PCM_16, but a different format would lead wrong calculation		
+		_duration_file = f.frames /  f.samplerate # see https://pysoundfile.readthedocs.io/en/latest/_modules/soundfile.html#info
+		_total_file_read_sec = 0.0 # keep memory of the file read in seconds
+		_print_debug(function='read_file_and_play', msg="file length: {} samples - {} sec.".format(f.frames, _duration_file))
+		for _n in range(audio_params.buffersize):
+			data = f.buffer_read(audio_params.blocksize, dtype='float32')
+			if not data:
+				#_print_debug(function='read_file_and_play', msg="break no data to prefill")
+				break
+			# _print_debug(function='read_file_and_play', msg="Prefill queue[{}] buffer_{} with {} len={}".format(data_queue, _n, data, len(data[:])))
+			data_queue.put_nowait(data)  # Pre-fill queue
+			#_print_debug(function='read_file_and_play', msg="Prefill queue[{}] qsize is {}".format(data_queue,data_queue.qsize()))
+		# At this point the prefilled buffer is _total_file_read_sec
+		# If the prebuffering is longer than the whole file the pre buffered part is the duration of the file
+		_total_file_read_sec = min(_duration_file * data_queue.qsize() * audio_params.blocksize/f.frames, _duration_file)		
+		_print_debug(function='read_file_and_play', msg="Prefill ended ---  prefilled {} sec. - buffer queue obj {},  -- now opening rawstream ".format(
+					 _total_file_read_sec, data_queue))
+		stream = sd.RawOutputStream(samplerate=f.samplerate, blocksize=audio_params.blocksize,
+									device=audio_params.device, channels=f.channels, dtype='float32',
+									callback=play_callback, finished_callback=play_finished) 
+
+		# update the value of duration per block, it is used in the callback
+		sample_per_block = round(stream.blocksize / (stream.channels * stream.samplesize))
+		duration_per_block = sample_per_block /  stream.samplerate
+		latency = stream.latency# / 2.0 # latgency of the output, roughly half
+		_print_debug(function='read_file_and_play', msg="Output Audio Stream opened: {} - device={} sr={}Hz,blocksize={},n_channels={},samplesize={} ".format(
+						stream, audio_params.device, stream.samplerate, stream.blocksize, stream.channels, stream.samplesize))
+		_print_debug(function='read_file_and_play', msg="stream data block length: {} samples - {} sec. - latency = {}".format(sample_per_block, duration_per_block, latency))
+		try:
+			with stream:
+				timeout = audio_params.blocksize * audio_params.buffersize/ _file_chunk_size / f.samplerate
+				_print_debug(function='read_file_and_play', 
+							 msg="START STREAM FROM FILE with a timeout {} sec. = blocksize({}) * buffersize({}) / samplerate({})".format(
+							 	  timeout, audio_params.blocksize, audio_params.buffersize, f.samplerate))
+				#_print_debug(function='read_file_and_play', msg="START STREAM FROM FILE with a timeout {} sec.".format(timeout))
+				data = f.buffer_read(audio_params.blocksize, dtype='float32')
+				while data:
+					data = f.buffer_read(audio_params.blocksize, dtype='float32')
+					#_print_debug(function='read_file_and_play', msg="stream from file data obj: {} - len {} bytes".format(data, len(data[:]) ))
+					time_chunk = _duration_file * len(data[:])/f.frames / _file_chunk_size
+					_total_file_read_sec += time_chunk				
+					_print_debug(function='read_file_and_play', msg="stream from file data chunk: {} len is {} sec., total read is {} sec.".format(data, time_chunk, _total_file_read_sec ))
+					try:
+						data_queue.put(data, timeout=timeout)
+					except queue.Full as e:
+						wait_ms = 1000*int(len(data[:])/f.samplerate)
+						_print_debug(function='read_file_and_play', msg='data available in file but queue is full... wait {} msec. and retry'.format(wait_ms))	
+						sd.sleep(wait_ms)
+						data_queue.put(data, timeout=timeout)
+					# _print_debug(function='read_file_and_play', msg="stream from file queue[{}] qsize is {}".format(data_queue,data_queue.qsize()))
+				_print_debug(function='read_file_and_play', msg="----- STOP STREAM FROM FILE! data are gone. Wait for event {}".format(event))
+				event.wait()  # Wait until playback is finished
+				_print_debug(function='read_file_and_play', msg="----- EXIT CLEAN - EVENT ALL DONE - ")
+		except Exception as e:
+			_print_debug(function='read_file_and_play', msg='---- END ERROR - Got exception in read_file_and_play:' + type(e).__name__ + ': ' + str(e))
+			raise e
+
+
+DEFAULT_PAUSE_BEFORE = 0.0 # silence before in sec.
+DEFAULT_PAUSE_AFTER = 0.75 # silence after in sec. 
+
+
+def get_wakeup_token(wave_folder, args):
+	ret_dict = {'filename':  '',
+				'expected_wakeup_word': '', 
+				'pause_before': DEFAULT_PAUSE_BEFORE, 
+				'pause_after': DEFAULT_PAUSE_AFTER}
+	for key, value in args.items():
+		print(key, value)
+		if key == 'filename':
+			value = os.path.join(wave_folder, value)        
+		ret_dict[key] = value
+	return ret_dict
+
+
+def get_intent_token(wave_folder, args):
+	ret_dict = {'filename':  '',
+				'expected_intents': [], 
+				'pause_before': DEFAULT_PAUSE_BEFORE, 
+				'pause_after': DEFAULT_PAUSE_AFTER}
+	for key, value in args.items():
+		if key == 'filename':
+			value = os.path.join(wave_folder, value)    
+		elif key == 'expected_intents':
+			if isinstance(value,str):
+				value = [value]
+		ret_dict[key] = value
+	return ret_dict
+
+
+def get_test_token(wave_folder, args_wakeup, args_intent):
+	print(args_wakeup)
+	return {'wakeup': get_wakeup_token(wave_folder, args_wakeup), 
+			'intent': get_intent_token(wave_folder, args_intent)}
+
+
+def get_json_test_token(wave_folder, args_wakeup, args_intent):
+	return json.dumps(get_test_token(wave_folder, args_wakeup, args_intent))
+
+	
